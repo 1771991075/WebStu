f1167ba7e7b9b422dbd9659451dd541cb5050408
lzc <lvzicong123@outlook.com>
2023-04-01 14:11:37 +0800

    vue核心语法

---
+++ vue/单文件组件/demo.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{name}}</h1>
+        <h1>{{address}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'School',
+        data(){
+            return {
+                name:'郑州大学',
+                address:'中原区'
+            }
+        }
+    }
+</script>
+
+<style>
+    
+</style>
